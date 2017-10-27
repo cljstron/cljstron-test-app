376aa785a45084d636ff3e1ca4443d9b62eb7d67
Ivan Pierre <me@ivanpierre.ch>
2017-10-27 11:46:56 +0200

    First commit

---
+++ src/main/main.cljs
@@ -0,0 +1,27 @@
+(ns main.main
+  (:require [cljstron.browser.window :refer [open-window]]
+            [cljs.nodejs :as node :refer [enable-util-print!]]
+            [electron :refer [app]]))
+
+(enable-util-print!)
+
+(defn- open-main-window []
+  "open the :main window"
+  (println "open window")
+  (open-window :cljstron.cljstron-simple/main
+    :js-urls     ["js/main.js"]
+    :main-fn     "cljstron_simple.renderer.main.init"
+    :win-conf    {}
+    :page-url    "plugs/cljstron-simple/resources/index.html"
+    :intro-html  "<h1>Hello World! (dev)</h1>
+    We are using node.js <script>document.write(process.version)</script>
+    and Electron <script>document.write(process.versions['electron'])</script>.")
+  (println "window opened"))
+    
+(defn ^:export init []
+  "`main` entry point."
+  (.on app "ready" open-main-window)  
+  (.on app "window-all-closed"
+    #(.quit app)))
+
+(init)
